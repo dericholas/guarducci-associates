faa7dc3e302aabeecb66a92580715f9ef5f26f22
Cornfrog <iamcornfrog@gmail.com>
2024-04-14 18:14:38 -0400

    added bav bar component to layout with working links

--- app/page.js
+++ app/page.js
@@ -1,8 +1,6 @@
 const Home = () => {
   return (
     <div>
-
-    <h2>Guarducci Associates</h2>
     <h1>WELCOME</h1>
     <h3>Guarducci Associates</h3>
     <p>Stained glass restoration and repair</p>
